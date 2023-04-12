70216bb1fcc70ade45743a8f5fac0f66b70968ad
Cristiano Cinotti <cristianocinotti@gmail.com>
2023-04-12 18:00:19 +0200

    Add to_gb filter (#2)
    Add to_gb filter

---
+++ src/Exception/TwigFilterException.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+/*
+ * Copyright (c) 2020 - 2023 Cristiano Cinotti
+ *
+ * This file is part of the Susina\TwigExtensions package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license Apache-2.0
+ */
+
+namespace Susina\TwigExtensions\Exception;
+
+use Twig\Error\Error;
+
+class TwigFilterException extends Error
+{
+}
